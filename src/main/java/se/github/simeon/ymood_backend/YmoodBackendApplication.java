deccc974561ac9816aef8c9f26f48d6115fdeaa7
SimeonFarkas <simeon.s.farkas@gmail.com>
2026-03-09 16:48:13 +0100

    Initial commit

---
+++ src/main/java/se/github/simeon/ymood_backend/YmoodBackendApplication.java
@@ -0,0 +1,13 @@
+package se.github.simeon.ymood_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmoodBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmoodBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/github/simeon/ymood_backend/YmoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.github.simeon.ymood_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmoodBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
